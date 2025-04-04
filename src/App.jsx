57d44d14bec063ef50fc694177f5c58b9df6ec19
divanshu sharma <divanshu1805@gmail.com>
2025-04-05 00:11:39 +0530

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <div style={{ height:"100vh", width:"100%" }} >
     <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
     <h1 id="eezib" style={{ fontFamily:"Raleway", fontSize:'3rem' }} >Eezib Technology pvt.ltd</h1>
+    <h4 id="eezib2" style={{ fontFamily:"Raleway", fontSize:'1rem' }} >Designed by Eezib tech @Divanshu..</h4>
     </div>
   )
 }
