f9a437f20a0728675d17eda3c855cc0e4b21a369
divanshu sharma <divanshu1805@gmail.com>
2025-04-06 01:28:54 +0530

    0123

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div style={{ height:"100vh", width:"100%" }} >
-    <Lanyard position={[0, 0, 20]} gravity={[0, -60, 0]} />
-    <h1 id="eezib" style={{ fontFamily:"Nura", fontSize:'3rem', fontWeight:500, textTransform:'capitalize' }} >Eezib Technology pvt.ltd</h1>
-    <h4 id="eezib2" style={{ fontFamily:"Raleway", fontSize:'1rem' }} >Designed by Eezib tech @Divanshu..</h4>
-    </div>
+    <div style={{ height:"100vh", width:"100%" }}>
+  <Lanyard position={[0, 0, 20]} gravity={[0, -60, 0]} />
+  <h1 id="eezib">Eezib Technology pvt.ltd</h1>
+  {/* <h4 id="eezib2">Designed by Eezib tech @Divanshu..</h4> */}
+</div>
+
   )
 }
 
